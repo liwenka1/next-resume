e2e8dd17645741b0b5e3853919010316c82359d5
liwenkai <2020583117@qq.com>
2024-04-18 21:30:38 +0800

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  endOfLine: 'lf'
+}

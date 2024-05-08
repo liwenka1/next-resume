5b24ed2304b51cc9cd5bdd07b0febde439ddd8e2
liwenkai <2020583117@qq.com>
2024-05-08 21:41:49 +0800

    fix: 实现最基本的富文本转canvas

--- components/editor-content.tsx
+++ components/editor-content.tsx
@@ -1,24 +1,52 @@
-import { useEffect } from 'react'
+import { createElement, useEffect, useMemo, useRef } from 'react'
 
-import { Plate, PlateContent } from '@udecode/plate-common'
+import { Plate } from '@udecode/plate-common'
 import html2canvas from 'html2canvas'
+import { useDebounceFn } from 'ahooks'
+
+import { Editor } from './plate-ui/editor'
 
 interface EditorContentProps {
   setCanvasImage: (Canvasmage: CanvasImageSource | null) => void
 }
 
 const EditorContent: React.FC<EditorContentProps> = ({ setCanvasImage }) => {
+  const initialValue = useMemo(
+    () => [
+      {
+        type: 'p',
+        children: [
+          {
+            text: 'This is editable plain text with react and history plugins, just like a <textarea>!'
+          }
+        ]
+      }
+    ],
+    []
+  )
+
   useEffect(() => {
     const element = document.getElementById('htmlElement')
     console.log(element, 'element')
 
-    // if (element) html2canvas(element).then((canvas) => setCanvasImage(canvas))
-  }, [setCanvasImage])
+    if (element) html2canvas(element).then((canvas) => setCanvasImage(canvas))
+  }, [setCanvasImage, initialValue])
+
+  const handleChange = (v: any) => {
+    console.log(v)
+
+    const element = document.getElementById('htmlElement')
+    console.log(element, 'element')
+
+    if (element) html2canvas(element).then((canvas) => setCanvasImage(canvas))
+  }
+
+  const { run } = useDebounceFn(handleChange, { wait: 500 })
 
   return (
-    <div className="htmlElement">
-      <Plate>
-        <PlateContent placeholder="Type..." />
+    <div id="htmlElement">
+      <Plate initialValue={initialValue} onChange={run}>
+        <Editor placeholder="Type..." />
       </Plate>
     </div>
   )
